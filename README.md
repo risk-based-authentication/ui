93d7812be310e20eb86a86c0acbbe1f46ade00c9
deniizeynep <zeynep351961@gmail.com>
2023-12-10 19:15:53 +0300

    project setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 ## RBA | Risk Based Authentication 
+
+## Available Scripts
+
+In the project directory, you can install:
+
+### `npm install`
+
+In the project directory, you can run:
+
+### `npm start`
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Payment from "./components/payment/Payment.tsx";
+
+function App() {
+  return (
+    <div>
+      <Payment />
+    </div>
+  );
+}
+
+export default App;
