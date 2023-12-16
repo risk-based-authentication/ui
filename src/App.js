5a40670fd98e3f21b9e34e28c4311edb351a905f
deniizeynep <zeynep351961@gmail.com>
2023-12-16 17:33:51 +0300

    Fail verification screen created

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Payment from "./components/payment/Payment.tsx";
 import Verification from "./components/verification/Verification.tsx";
 import SuccessVerification from "./components/verification/SuccessVerification.tsx";
+import FailVerification from "./components/verification/FailVerification.tsx";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Payment />} />
         <Route path="verification" element={<Verification />} />
         <Route path="success" element={<SuccessVerification />} />
+        <Route path="fail" element={<FailVerification />} />
       </Routes>
     </BrowserRouter>
   );
